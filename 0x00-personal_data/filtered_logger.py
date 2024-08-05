c2b89c659dd8bb5b3fe3a8aee9fac2c1a72c08d2
Gideon Oba <deyonoba@gmail.com>
2024-08-05 09:14:33 +0100

    modified: filtered_logger.py file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,28 @@ message obfuscated.
 The function uses regex to replace occurrences of certain field values.
 """
 import re
+import logging
 from typing import List
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Implement the format method to filter values
+        in incoming log records using filter_datum.
+        """
+        
 
 
 def filter_datum(
@@ -34,16 +55,3 @@ def filter_datum(
         pattern = f"({field}=)([^{separator}*])"
         message = re.sub(pattern, r"\1" + redaction, message)
     return message
-
-
-if __name__ == "__main__":
-    fields = ["password", "date_of_birth"]
-    messages = [
-        "name=egg;email=eggmin@eggsample.com;password=eggcellent;\
-date_of_birth=12/12/1986;",
-        "name=bob;email=bob@dylan.com;password=bobbycool;\
-date_of_birth=03/04/1993;"
-    ]
-
-    for message in messages:
-        print(filter_datum(fields, 'xxx', message, ';'))
