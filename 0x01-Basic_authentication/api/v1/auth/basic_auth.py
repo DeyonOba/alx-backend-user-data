35fc70cb7e96e5801b4624c6b4c4386a5113743d
Gideon Oba <deyonoba@gmail.com>
2024-08-07 17:35:47 +0100

    refactor: revert back to regex solution

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,19 +14,19 @@ class BasicAuth(Auth):
         Gets the Base64 part of the Authorization header
         for a Basic Authentication.
         """
-        # pattern = r"Basic\s+(.+)"
-        # PATTERN = re.compile(pattern)
-        # if type(authorization_header) is str:
-        #     match = re.match(PATTERN, authorization_header)
-        #     if match:
-        #         return match.group(1)
-        # return None
+        pattern = r"Basic\s+(.+)"
+        PATTERN = re.compile(pattern)
+        if type(authorization_header) is str:
+            match = re.match(PATTERN, authorization_header)
+            if match:
+                return match.group(1)
+        return None
         # Solution below is still correct but I wanted to try out
         # regex instead
-        if (
-            type(authorization_header) is str and
-            authorization_header.startswith("Basic ") and
-            authorization_header.count(" ") == 1
-        ):
-            return authorization_header.split(" ")[-1].strip()
-        return None
+        # if (
+        #     type(authorization_header) is str and
+        #     authorization_header.startswith("Basic ") and
+        #     authorization_header.count(" ") == 1
+        # ):
+        #     return authorization_header.split(" ")[-1].strip()
+        # return None
