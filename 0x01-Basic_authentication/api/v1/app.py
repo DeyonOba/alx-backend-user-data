8a226f934f65c2cedd10956d1cfaaad727fba891
Gideon Oba <deyonoba@gmail.com>
2024-08-07 11:29:23 +0100

    modified: app.py file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,14 +4,43 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, logging
 from flask_cors import (CORS, cross_origin)
 import os
-
+import sys
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if os.getenv('AUTH_TYPE'):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def handle_before_request():
+    # print("Handle authorization before request.")
+
+    if auth:
+        print("Auth request founded")
+        exclude_path = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+        ]
+        path = auth.require_auth(request.path, exclude_path)
+        # print(f"{path=}")
+        if path:
+            auth_type = auth.authorization_header(request)
+            # print(f"{auth_type=}")
+            if not auth_type:
+                abort(401)
+            current_user = auth.current_user(request)
+            # print(f"{current_user=}")
+            if not current_user:
+                abort(403)
 
 
 @app.errorhandler(401)
