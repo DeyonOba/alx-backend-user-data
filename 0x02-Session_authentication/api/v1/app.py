b382bc0c22a0ccce81f428160b09da697e7d4d1f
Gideon Oba <deyonoba@gmail.com>
2024-08-11 00:52:57 +0100

    update api/v1/app.py add SessionAuth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if auth_type == "auth":
 elif auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
@@ -47,6 +50,7 @@ def handle_before_request():
             # print(f"{current_user=}")
             if not current_user:
                 abort(403)
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(401)
