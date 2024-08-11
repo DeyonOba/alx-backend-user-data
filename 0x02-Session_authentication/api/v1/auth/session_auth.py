5b513ac9d093fa7237d213584583b7e9ce07e5f5
Gideon Oba <deyonoba@gmail.com>
2024-08-11 03:16:43 +0100

    feat: Add function user_id_for_session_id to SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,3 +23,11 @@ class SessionAuth(Auth):
             self.user_id_by_session_id.update({session_id: user_id})
             return session_id
         return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieves user ID based on a session ID.
+        """
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
+        return None
