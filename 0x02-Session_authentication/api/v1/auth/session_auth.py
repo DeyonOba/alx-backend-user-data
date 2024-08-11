63999246aab2d2c606c40bd8de77e8434e1b8598
Gideon Oba <deyonoba@gmail.com>
2024-08-11 02:58:59 +0100

    feat: Add function create_session to SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import re
+import uuid
 import base64
 from typing import Tuple, Union, TypeVar
 from models.user import User
@@ -10,12 +11,15 @@ import hashlib
 
 
 class SessionAuth(Auth):
-    """Class BasicAuth"""
-    def extract_base64_authorization_header(
-         self, authorization_header: str
-    ) -> str:
+    """Class Session Auth"""
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
         """
-        Gets the Base64 part of the Authorization header
-        for a Session Authentication.
+        Creates a Session ID for a `user_id`.
         """
-        pass
+        if type(user_id) is str:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id.update({session_id: user_id})
+            return session_id
+        return None
